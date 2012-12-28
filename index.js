5d7bfbe3f002ded9226b0ca77f3c89fef83a26f9
E.Azer Koçulu <azer@kodfabrik.com>
2012-12-28 15:49:52 -0800

    0.0.3

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = pubsub;
-module.exports.on = on;
 
 function pubsub(customProxy){
   var proxy = customProxy || function pubsubProxy(){
@@ -92,63 +91,4 @@ function unsubscribe(to, callback){
   }
 
   return false;
-}
-
-function on(/* pubsubs..., callback */){
-  var callback      = arguments[arguments.length - 1],
-      subscriptions = [],
-      fired         = [],
-      timer;
-
-  function newSubscriber(pubsub){
-    return function(){
-      var args  = arguments;
-
-      fired.push({ pubsub: pubsub, params: args });
-
-      if(timer){
-        clearTimeout(timer);
-        timer = undefined;
-      }
-
-      timer = setTimeout(function(){
-        callback(fired);
-        fired = [];
-      }, 0);
-    };
-  }
-
-  function add(){
-    var i = arguments.length,
-        cb;
-
-    while( i --> 0 ){
-      cb = newSubscriber(arguments[i]);
-      arguments[i].subscribe(cb);
-
-      subscriptions.push({
-        pubsub: arguments[i],
-        callback: cb
-      });
-    }
-  }
-
-  function rm(pubsub){
-    var i = subscriptions.length,
-        removed = false;
-
-    while( i --> 0 ){
-      if(subscriptions[i] && subscriptions[i].pubsub == pubsub){
-        pubsub.unsubscribe(subscriptions[i].callback);
-        subscriptions[i] = undefined;
-      }
-    }
-  }
-
-  add.apply(undefined, Array.prototype.slice.call(arguments, 0, arguments.length - 1));
-
-  return {
-    subscribeTo: add,
-    unsubscribeTo: rm
-  };
 }
